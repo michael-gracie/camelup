350eb4d2491a05ee8ff4a7e3e9b228f58e48d65f
Michael Gracie <52887646+michael-gracie@users.noreply.github.com>
2019-08-04 15:42:18 -0400

    Finishing unit testing for camelup

--- camelup/camelup.py
+++ camelup/camelup.py
@@ -40,27 +40,26 @@ class Game:
         """
         base = {"height": None, "space": None, "need_roll": True}
         for camel in config.CAMELS:
-            self.camel_dict[camel] = base
+            self.camel_dict[camel] = deepcopy(base)
 
     def _gen_player_dict(self):
         """Generates base player dictionary
         """
         base = {
-            "winner_cards": config.CAMELS,
-            "loser_cards": config.CAMELS,
+            "game_cards": deepcopy(config.CAMELS),
             "tile": True,
             "coins": config.COINS,
             "bet_tiles": dict(),
         }
         for player in range(self.num_players):
-            self.player_dict[player + 1] = base
+            self.player_dict[player + 1] = deepcopy(base)
 
     def _gen_bet_tiles(self):
         """Generates base bet tile configuration
         """
         base = [5, 3, 2]
         for camel in config.CAMELS:
-            self.bet_tiles[camel] = base
+            self.bet_tiles[camel] = deepcopy(base)
 
     def end_game(self):
         """Ends the game
@@ -69,28 +68,28 @@ class Game:
         3. Scores the loser bets
         """
         self.score_round()
-        order = list(self._winner(self.camel_dict.keys()))
+        order = list(self._winner(self.camel_dict).keys())
         winner = order[0]
         loser = order[-1]
-        rewards = config.BET_POINTS
+        rewards = deepcopy(config.BET_POINTS)
         for bet in self.winner_bets:
-            if bet[0] == winner:
+            if bet[1] == winner:
                 if rewards:
-                    self.player_dict[bet[1]]["coins"] += rewards.pop(0)
+                    self.player_dict[bet[0]]["coins"] += rewards.pop(0)
                 else:
-                    self.player_dict[bet[1]]["coins"] += 1
+                    self.player_dict[bet[0]]["coins"] += 1
             else:
-                self.player_dict[bet[1]]["coins"] -= 1
-        rewards = config.BET_POINTS
+                self.player_dict[bet[0]]["coins"] -= 1
+        rewards = deepcopy(config.BET_POINTS)
         for bet in self.loser_bets:
-            if bet[0] == loser:
+            if bet[1] == loser:
                 if rewards:
-                    self.player_dict[bet[1]]["coins"] += rewards.pop(0)
+                    self.player_dict[bet[0]]["coins"] += rewards.pop(0)
                 else:
-                    self.player_dict[bet[1]]["coins"] += 1
+                    self.player_dict[bet[0]]["coins"] += 1
             else:
-                self.player_dict[bet[1]]["coins"] -= 1
-        pass
+                self.player_dict[bet[0]]["coins"] -= 1
+        return "Done"
 
     def end_round(self):
         """The end of the round triggers the following
@@ -114,16 +113,15 @@ class Game:
         If the camel is in first then the player gets full points.
         If in second they get 1 point, else they lose a point
         """
-        order = list(self._winner(self.camel_dict.keys()))
-        for player in self.player_dict:
-            tiles = player["bet_tiles"]
-            for key, val in tiles.items():
+        order = list(self._winner(self.camel_dict).keys())
+        for player, player_val in self.player_dict.items():
+            for key, val in player_val["bet_tiles"].items():
                 if key == order[0]:
                     self.player_dict[player]["coins"] += np.sum(val)
-                if key == order[1]:
-                    self.player_dict[player]["coins"] += np.count(val)
+                elif key == order[1]:
+                    self.player_dict[player]["coins"] += np.size(val)
                 else:
-                    self.player_dict[player]["coins"] -= np.count(val)
+                    self.player_dict[player]["coins"] -= np.size(val)
 
     def available_moves(self):
         """Computes all the moves that are available.
@@ -134,37 +132,24 @@ class Game:
             Moves available to play
         """
         moves = dict()
-        for card in self.player_dict[self.state]["winner_cards"]:
+        for card in self.player_dict[self.state]["game_cards"]:
             moves[f"Bet Game Winner {card}"] = f"self.play_winner_card({card})"
-        for card in self.player_dict[self.state]["loser_cards"]:
+        for card in self.player_dict[self.state]["game_cards"]:
             moves[f"Bet Game Loser {card}"] = f"self.play_loser_card({card})"
         for camel in self.bet_tiles:
             moves[
                 f"Bet Round Winner {camel} - {self.bet_tiles[camel][0]} Points"
             ] = f"self.play_bet_tile({camel})"
-        moves["Roll"] = "self.roll(camel, roll)"
+        moves["Roll"] = "self.play_roll(camel, roll)"
         if self.player_dict[self.state]["tile"]:
-            for spot in self.available_tiles_placements():
+            for spot in self.available_tile_placements():
                 moves[
                     f"Place Block Tile At {spot}"
                 ] = f"self.play_tile('block', {spot})"
                 moves[f"Place Skip Tile At {spot}"] = f"self.play_tile('skip', {spot})"
         return moves
 
-    def play(self, move):
-        """Play the move and update state
-
-        Parameters
-        ----------
-        move : 'str'
-            The move to play, formatted as a function
-        """
-        eval(move)
-        self.state += 1
-        if self.state > self.num_players:
-            self.state = 1
-
-    def available_tiles_placements(self):
+    def available_tile_placements(self):
         """Compute the available tiles
 
         Returns
@@ -181,11 +166,28 @@ class Game:
         for camel in self.camel_dict.values():
             blocked.append(camel["space"])
         spots = list(range(1, 17))
-        spaces = [spot for spot in spots if spot not in blocked & spot > min(blocked)]
+        spaces = [
+            spot for spot in spots if (spot not in blocked) & (spot > min(blocked))
+        ]
         return spaces
 
+    def play(self, move):
+        """
+        1. Plays the move
+        2. Update state
+
+        Parameters
+        ----------
+        move : 'str'
+            The move to play, formatted as a function
+        """
+        eval(move)
+        self.state += 1
+        if self.state > self.num_players:
+            self.state = 1
+
     def play_tile(self, tile_type, space):
-        """Play a skip or block tile
+        """Plays a skip or block tile
 
         Parameters
         ----------
@@ -205,7 +207,7 @@ class Game:
         camel : str
             The camel to bet on a win
         """
-        self.player_dict[self.state]["winner_cards"].remove[camel]
+        self.player_dict[self.state]["game_cards"].remove(camel)
         self.winner_bets.append((self.state, camel))
 
     def play_loser_card(self, camel):
@@ -216,7 +218,7 @@ class Game:
         camel : str
             The camel to bet on a loss
         """
-        self.player_dict[self.state]["loser_cards"].remove[camel]
+        self.player_dict[self.state]["game_cards"].remove(camel)
         self.loser_bets.append((self.state, camel))
 
     def play_bet_tile(self, camel):
@@ -234,8 +236,14 @@ class Game:
         if self.bet_tiles[camel] == []:
             self.bet_tiles.pop(camel, None)
 
-    def roll(self, camel, roll):
-        """Moves the camel and updates scores based on a roll
+    def play_roll(self, camel, roll):
+        """
+        1. Moves the camel
+        2. Give points if camel landed on a block or skip tile
+        3. Gives a point to the roller
+        4. Updates needs roll
+        5. Checks for end of game
+        6. Checks for end of round
 
         Parameters
         ----------
@@ -244,10 +252,16 @@ class Game:
         roll : int
             The number that is rolled
         """
-        gameplay.move(self.camel_dict, self.tiles_dict, camel, roll)
+        give_points = gameplay.move(self.camel_dict, self.tiles_dict, camel, roll)
+        if give_points:
+            tile_owner = self.tiles_dict[give_points]["player"]
+            self.player_dict[tile_owner]["coins"] += 1
         self.player_dict[self.state]["coins"] += 1
-        if self.camel_dict["camel"]["space"] > 16:
+        self.camel_dict[camel]["need_roll"] = False
+        if self.camel_dict[camel]["space"] > 16:
             self.end_game()
+        if any([val["need_roll"] for val in self.camel_dict.values()]) is False:
+            self.end_round()
 
     def _winner(self, camel_dict):
         """
--- tests/test_camelup.py
+++ tests/test_camelup.py
@@ -3,6 +3,8 @@
 
 """Tests for `camelup` package."""
 
+from copy import deepcopy
+
 import pytest
 
 from camelup import camelup
@@ -15,10 +17,128 @@ camel_dict = {
 }
 
 
-@pytest.fixture
-def game():
-    game = camelup.Game()
+@pytest.fixture(params=[2, 3, 4])
+def game_param(request):
+    num_players = request.param
+    game = camelup.Game(num_players)
     return game
+
+
+@pytest.fixture
+def camel_dict_copy():
+    return deepcopy(camel_dict)
+
+
+@pytest.fixture()
+def game(camel_dict_copy):
+    game = camelup.Game(3)
+    game.camel_dict = camel_dict_copy
+    return game
+
+
+@pytest.mark.parametrize(
+    "game_param,expected", [(2, 2), (3, 3), (4, 4)], indirect=["game_param"]
+)
+def test_gen_player_dict(game_param, expected):
+    assert list(game_param.player_dict.keys()) == list(range(1, expected + 1))
+
+
+def test_end_game(game):
+    game.state = 1
+    game.play_winner_card("red")
+    game.play_winner_card("blue")
+    game.play_winner_card("green")
+    game.state = 3
+    game.play_loser_card("red")
+    game.play_winner_card("green")
+    game.end_game()
+    assert game.player_dict[1]["coins"] == 11
+    assert game.player_dict[2]["coins"] == 5
+    assert game.player_dict[3]["coins"] == 18
+
+
+def test_end_round(game):
+    game.tiles_dict = {1: 3}
+    game.end_round()
+    assert game.tiles_dict == dict()
+
+
+def test_score_round(game):
+    game.state = 1
+    game.play_bet_tile("green")
+    game.play_bet_tile("red")
+    game.state = 3
+    game.play_bet_tile("green")
+    game.score_round()
+    assert game.player_dict[1]["coins"] == 9
+    assert game.player_dict[2]["coins"] == 5
+    assert game.player_dict[3]["coins"] == 8
+
+
+def test_available_moves(game):
+    """
+    14 * 2 tile placements
+    5 bet tiles
+    5 loser bets
+    5 winner bets
+    1 roll
+    """
+    assert len(game.available_moves()) == 44
+
+
+def test_available_tile_placements(game):
+    assert game.available_tile_placements() == list(range(3, 17))
+    game.play_tile("block", 3)
+    assert game.available_tile_placements() == list(range(5, 17))
+    for space in list(range(3, 17)):
+        game.play_tile("block", space)
+    assert game.available_tile_placements() == list()
+
+
+def test_play(game):
+    game.state = 1
+    game.play("self.play_tile('block', 5)")
+    assert game.state == 2
+    game.play("self.play_tile('block', 7)")
+    assert game.state == 3
+    game.play("self.play_tile('block', 9)")
+    assert game.state == 1
+    assert list(game.tiles_dict.keys()) == [5, 7, 9]
+
+
+def test_play_tile(game):
+    game.play_tile("block", 5)
+    assert game.tiles_dict == {5: {"tile_type": "block", "player": 1}}
+
+
+def test_play_winner_card(game):
+    game.play_winner_card("red")
+    assert game.winner_bets == [(1, "red")]
+
+
+def test_play_loser_card(game):
+    game.play_loser_card("red")
+    assert game.loser_bets == [(1, "red")]
+
+
+def test_play_bet_tile(game):
+    game.play_bet_tile("red")
+    assert game.player_dict[1]["bet_tiles"] == {"red": [5]}
+    game.play_bet_tile("red")
+    game.play_bet_tile("red")
+    assert game.player_dict[1]["bet_tiles"] == {"red": [5, 3, 2]}
+    assert "red" not in game.bet_tiles.keys()
+
+
+def test_play_roll(game):
+    game.state = 1
+    game.play_tile("block", 3)
+    game.play_roll("green", 1)
+    assert game.player_dict[1]["coins"] == 7
+    assert game.camel_dict["green"]["need_roll"] == False
+    game.play_winner_card("green")
+    game.play_roll("green", 17)
+    assert game.player_dict[1]["coins"] == 16
 
 
 def test_winner(game):
